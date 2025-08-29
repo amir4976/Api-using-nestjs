61528635a51424f8517f82d69b495cd258cb6aa0
amir4976 <amirhosain4976@gmail.com>
2025-08-29 10:14:03 +0330

    add security

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,23 @@ import { UsersModule } from './users/users.module';
 import { DatabaseService } from './database/database.service';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    EmployeesModule,
+
+    //? for better security add a guard to check the request rate
+    //? many attacks are like send 10000 requests in a second
+    //? so we need to limit the number of requests to a certain time period 
+    //* yeh fuck u hackers 😎
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+
+    
+  ],
   providers: [DatabaseService],
 })
 export class AppModule {}
- 
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // so we can set a prefix route for all the routs like 
+  //localhost:3500/api/... 
+  app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3500);
 }
 bootstrap();
